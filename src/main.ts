d70929bc77f5042ddea8f4a8437d3efeeda362b3
M. Aji Perdana <ajicooljazz38@gmail.com>
2024-08-16 15:01:31 +0700

    feat: init hands-on nest with keycloak sso ✨

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestApplication, NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap(): Promise<void> {
+  const port: string | number = process.env.port || 5500;
+  const globalPrefix: string = 'api/v1';
+
+  const app: NestApplication =
+    await NestFactory.create<NestApplication>(AppModule);
+
+  app.enableCors();
+  app.setGlobalPrefix(globalPrefix);
+  await app.listen(port, '0.0.0.0');
+  Logger.log(
+    `🚀 Application listening on http://0.0.0.0:${port}/${globalPrefix}`,
+  );
+}
+
+bootstrap();
